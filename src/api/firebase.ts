b347135b4b5bda96ec3908d3434bbe82cec5929f
Jonathan Gruber <gruberjonathan@gmail.com>
2020-02-04 23:05:49 +0100

    Add Google Analytics

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,39 +1,35 @@
 import firebase from 'firebase/app';
 
+import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const config = {
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   appId: process.env.REACT_APP_APP_ID,
-};
-
-const app = firebase.initializeApp(config);
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
 
 const auth = app.auth();
 auth.languageCode = 'de';
 
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-
+const analytics = app.analytics();
 const firestore = app.firestore();
 
 function timestamp(date: Date = new Date()) {
   return new firebase.firestore.Timestamp(
-    date.getTime() / 1000, // returns ms
+    date.getTime() / 1000, // getTime() returns ms
     0,
   );
 }
 
-export function renewPasswords(password: string) {
-  return auth.currentUser?.updatePassword(password);
-}
-
 export default {
+  analytics,
   auth,
-  googleAuthProvider,
   firestore,
+  googleAuthProvider: new firebase.auth.GoogleAuthProvider(),
   timestamp,
 };
