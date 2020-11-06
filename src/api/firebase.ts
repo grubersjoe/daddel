a60711dbf5b85fed5e4b981b18af2359743edcb7
Jonathan Gruber <jogu@spreadshirt.net>
2020-11-06 18:45:30 +0100

    Get rid of unnecessary env variable

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -7,7 +7,6 @@ import 'firebase/firestore';
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
