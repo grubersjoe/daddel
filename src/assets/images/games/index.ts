ee76bb0b13d4a4dc396141714fa2b62e2f9235e0
Jonathan Gruber <gruberjonathan@gmail.com>
2021-01-15 12:43:54 +0100

    This cache is not working

--- src/assets/images/games/index.ts
+++ src/assets/images/games/index.ts
@@ -3,25 +3,13 @@ import slugify from '@sindresorhus/slugify';
 import { Game } from '../../../types';
 import { supportsWebp } from '../../../utils';
 
-const cache: {
-  [key: string]: string | null;
-} = {};
-
 export async function getGameBanner(game: Game): Promise<string | null> {
-  if (!game.name) {
-    throw new Error('Game name not set');
-  }
-
   const slug = slugify(game.name, { separator: '-' });
   const fileName = slug.concat((await supportsWebp()) ? '.webp' : '.jpg');
 
-  if (cache[fileName] === undefined) {
-    try {
-      cache[fileName] = (await import(`./out/${fileName}`)).default;
-    } catch {
-      cache[fileName] = null;
-    }
+  try {
+    return (await import(`./out/${fileName}`)).default;
+  } catch {
+    return null;
   }
-
-  return cache[fileName];
 }
