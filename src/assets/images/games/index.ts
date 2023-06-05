b3e60a9f4e0c0598709fbcb552c7a445bbd98f77
Jonathan Gruber <gruberjonathan@gmail.com>
2023-06-05 22:13:05 +0200

    Remove manually maintained image files

--- src/assets/images/games/index.ts
+++
@@ -1,6 +0,0 @@
-export async function getGameBanner(steamAppId: number) {
-  const fileName = String(steamAppId).concat('.webp');
-  const url = new URL(`./out/${fileName}`, import.meta.url);
-
-  return url.pathname === '/undefined' ? null : url;
-}
