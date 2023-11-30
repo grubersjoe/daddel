ab2917e4ca3d7bd60ba935072ced1d060b2aa1ab
Jonathan Gruber <jonathan.gruber@gridfuse.com>
2023-11-30 10:01:28 +0100

    Add missing env variables to check

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -1,16 +1,18 @@
 import { FirebaseOptions } from 'firebase/app';
 
-const configKeys = [
+const variableNames = [
   'VITE_API_KEY',
   'VITE_APP_ID',
   'VITE_AUTH_DOMAIN',
+  'VITE_DOMAIN_PROD',
   'VITE_MEASUREMENT_ID',
   'VITE_MESSAGING_SENDER_ID',
   'VITE_PROJECT_ID',
+  'VITE_STEAM_AUTH_API',
   'VITE_VAPID_KEY',
 ];
 
-configKeys.forEach(key => {
+variableNames.forEach(key => {
   if (!import.meta.env[key]) {
     throw new Error(`Environmental variable ${key} is not set`);
   }
